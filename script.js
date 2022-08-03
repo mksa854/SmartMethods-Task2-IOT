5a1b80f53dbe77e04522ee0fe10bd259c5041764
mksa854 <107880411+mksa854@users.noreply.github.com>
2022-08-04 00:17:54 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+var output = document.getElementById("output");
+var start = document.getElementById("start");
+var stop = document.getElementById("stop");
+var language_Rec = document.getElementById("language_Rec");
+var speechRecognition = speechRecognition || webkitSpeechRecognition;
+var recognizer = new speechRecognition();
+
+recognizer.lang = 'ar';
+
+language_Rec.onchange = function() {
+    recognizer.lang = language_Rec.value;
+}
+
+//controls
+start.onclick = function() {
+    recognizer.start();
+}
+stop.onclick = function() {
+    recognizer.stop();
+}
+recognizer.onspeechend = function() {
+    stop.click();
+}
+// to write:
+recognizer.onresult = function(event) {
+    var Textbox = event.results[0][0].transcript;
+    output.value = Textbox;
+    getCommand();
+}
